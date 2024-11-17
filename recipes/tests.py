3eb82e2e77ea98a2ee806ae83c221489041b8804
nedarctic <nedarctic@gmail.com>
2024-11-17 15:55:45 +0300

    Added tests for the about page

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,7 +1,7 @@
 from django.test import SimpleTestCase
 from django.urls import resolve, reverse
 
-from .views import HomePageView
+from .views import HomePageView, AboutPageView
 
 class HomePageTests(SimpleTestCase):
     def setUp(self):
@@ -19,3 +19,18 @@ class HomePageTests(SimpleTestCase):
     def test_homepage_resolves_correct_view(self):
         view = resolve('/')
         self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
+class AboutPageTests(SimpleTestCase):
+    def setUp(self):
+        url = reverse("about")
+        self.response = self.client.get(url)
+    def test_about_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+    def test_about_correct_template(self):
+        self.assertTemplateUsed(self.response, "about.html")
+    def test_about_correct_html(self):
+        self.assertContains(self.response, "About Page")
+    def test_about_incorrect_html(self):
+        self.assertNotContains(self.response, "I should not be on the page")
+    def test_about_resolves_correct_view(self):
+        view = resolve("/about/")
+        self.assertEqual(view.func.__name__, AboutPageView.as_view().__name__)
