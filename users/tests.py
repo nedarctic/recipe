5319976c96cf8b4c76f137ccc76e05e958b38891
nedarctic <nedarctic@gmail.com>
2024-11-17 10:05:58 +0300

    Created registration folder and added signup templates together with tests for the sigup functionality

--- users/tests.py
+++ users/tests.py
@@ -1,5 +1,9 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.urls import reverse, resolve
+
+from .forms import CustomUserCreationForm
+from .views import SignupPageView
 
 class CustomUserTests(TestCase):
     def test_create_user(self):
@@ -27,3 +31,23 @@ class CustomUserTests(TestCase):
         self.assertTrue(admin.is_staff)
         self.assertTrue(admin.is_superuser)
         self.assertTrue(admin.is_active)
+        
+class SignupPageTests(TestCase):
+    def setUp(self):
+        url = reverse("signup")
+        self.response = self.client.get(url)
+        
+    def test_signup_page_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+    def test_signup_page_uses_correct_template(self):
+        self.assertTemplateUsed(self.response, "signup.html")
+    def test_signup_page_contains_correct_html(self):
+        self.assertContains(self.response, "Sign Up")
+    def test_signup_page_does_not_contain_correct_html(self):
+        self.assertNotContains(self.response, "I should not be on the page")
+    def test_signup_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, CustomUserCreationForm)
+    def test_signup_resolves_correct_view(self):
+        view = resolve("/accounts/signup/")
+        self.assertEqual(view.func.__name__, SignupPageView.as_view().__name__)
