ac9679f58385efc44c9104a4bf7004cbae93b529
nedarctic <nedarctic@gmail.com>
2024-11-18 17:58:35 +0300

    Added environment variables

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2e%cv8#27v!1-=m@uw_hus%ap%+@(q@j6_ur)ynes_q_$qa3cb'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = []
 
